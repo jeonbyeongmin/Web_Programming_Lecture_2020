e87c23f8607ffc2a0d465185f08f1cfce0007ce9
jeonbyeongmin <qudals7613@gmail.com>
2020-10-01 02:57:39 +0900

    1차 완료 :: 검토 시작

--- hw1/popUpForm.js
+++ hw1/popUpForm.js
@@ -13,7 +13,13 @@ let _buttonEalryMorningSectionRemove = document.getElementById("earlyMorningSect
 // 이동 버튼
 let _buttonMoveToMorningDelivery = document.getElementById("moveToMorningDelivery");
 let _buttonMoveToNormalDelivery = document.getElementById("moveToNormalDelivery");
-
+// 검색버튼
+let _buttonSearchProducts = document.getElementById("searchProducts");
+let _buttonBackToNormal = document.getElementById("backToNormal");
+// 검색 텍스트 박스
+let _textBoxInputProductsName = document.getElementById("inputProductsName");
+let _textBoxInputMinPrice = document.getElementById("inputMinPrice");
+let _textBoxInputMaxPrice = document.getElementById("inputMaxPrice");
 
 // 유효성 검사
 var _onlyNumber = /^[0-9]*$/; // 숫자 유효성 검사를 위한 정규식
@@ -62,23 +68,30 @@ _buttonNormalSectionRemove.addEventListener("click", function() {
   deleteRow(_normalCheckBox, _normalDeliveryInfoTable, _normalContent);
   sumTotalPrice(_normalCheckBox, _normalContent, _normalTotalPrice);
   isAllCheck(_normalCheckBox, _normalAllCheck);
-})
+});
 
 _buttonEalryMorningSectionRemove.addEventListener("click", function() {
   deleteRow(_earlyMorningCheckBox, _earlyMorningDeliveryInfoTable, _earlyMorningContent);
   sumTotalPrice(_earlyMorningCheckBox, _earlyMorningContent, _earlyMorningTotalPrice);
   isAllCheck(_earlyMorningCheckBox, _earlyMorningAllCheck);
-})
+});
 
 _buttonMoveToMorningDelivery.addEventListener("click", function() { // 새벽 배송으로 이동
+  moveToNormalDelivery();
+  isAllCheck(_normalCheckBox, _normalAllCheck);
+  sumTotalPrice(_normalCheckBox, _normalContent, _normalTotalPrice);
+});
 
-})
 _buttonMoveToNormalDelivery.addEventListener("click", function() { // 일반배송으로 이동
+  moveToMorningDelivery();
+  sumTotalPrice(_earlyMorningCheckBox, _earlyMorningContent, _earlyMorningTotalPrice);
+  isAllCheck(_earlyMorningCheckBox, _earlyMorningAllCheck);
+});
 
-})
+_buttonSearchProducts.addEventListener("click", searchProducts);
+_buttonBackToNormal.addEventListener("click", backToNormal);
 
-
-/****************************** CHECK BOX ************************************/
+/*************************** CHECKBOX CLICK *********************************/
 
 _normalAllCheck.addEventListener("click", function() {
   allCheck(_normalAllCheck, _normalCheckBox, _normalContent, _normalTotalPrice);
@@ -92,8 +105,6 @@ _earlyMorningAllCheck.addEventListener("click", function() {
 
 
 /******************************* FUNCTION ***********************************/
-
-
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
@@ -285,6 +296,9 @@ function isAllCheck(whatTypeCheckBox, whatTypeAllCheck) {
   } else {
     whatTypeAllCheck.checked = false;
   }
+  if (count == 0) { // 콘텐츠가 없으면 전체선택 해제
+    whatTypeAllCheck.checked = false;
+  }
 }
 
 //전체선택을 누르면 해당영역의 모든 체크박스가 해제되는 함수
@@ -332,10 +346,299 @@ function deleteRow(whatTypeCheckBox, whatTypeTable, whatContent){
 
 // 콘텐츠를 새벽배송에서 일반배송으로 이동시키는 함수
 function moveToNormalDelivery() {
+  for (var i = 0; i < _normalContent.length;) {
+    if (_normalCheckBox[i].checked == true) {
+      // row
+      var addRow = _earlyMorningDeliveryInfoTable.insertRow(_earlyMorningDeliveryInfoTable.rows.length);
+      addRow.className += "earlyMorningContent"; // row가 추가될 때마다 해당 row에 content라는 class 명을 붙여준다.
 
+      // cell_1 :: checkbox :: 체크된 채로 생성
+      var cell1= addRow.insertCell(0);
+      let addCheckbox = document.createElement("input");
+      addCheckbox.className = "earlyMorningDeliveryInfoTableCheckBox";
+      addCheckbox.type = "checkbox";
+      addCheckbox.checked = true;
+      addCheckbox.addEventListener('click', function() {
+        sumTotalPrice(_earlyMorningCheckBox, _earlyMorningContent, _earlyMorningTotalPrice);
+        isAllCheck(_earlyMorningCheckBox, _earlyMorningAllCheck);
+      })
+      cell1.appendChild(addCheckbox);
+
+
+      // cell_2 :: 상품 이미지
+      var cell2 = addRow.insertCell(1);
+      cell2.innerHTML = _normalContent[i].children[1].innerHTML;
+
+      // cell_3 :: 상품 이름
+      var cell3 = addRow.insertCell(2);
+      cell3.innerHTML = _normalContent[i].children[2].innerText;
+
+      // cell_4 :: 상품 개별 가격
+      var cell4 = addRow.insertCell(3);
+      cell4.innerHTML = _normalContent[i].children[3].innerText;
+
+      // cell_5 :: 상품 개수
+      var cell5 = addRow.insertCell(4);
+      cell5.innerHTML = _normalContent[i].children[4].innerText;
+
+      // cell_6 :: 삼품 구매 가격
+      var cell6 = addRow.insertCell(5);
+      cell6.innerHTML = parseInt(cell4.innerText) * parseInt(cell5.innerText);
+
+      // 전체선택 체크
+      sumTotalPrice(_earlyMorningCheckBox, _earlyMorningContent, _earlyMorningTotalPrice);
+      isAllCheck(_earlyMorningCheckBox, _earlyMorningAllCheck);
+
+      _normalDeliveryInfoTable.deleteRow(i);
+    } else {
+      i++;
+    }
+  }
 }
 
 // 콘텐츠를 일반배송에서 새벽배송으로 이동시키는 함수
 function moveToMorningDelivery() {
+  for (var i = 0; i < _earlyMorningContent.length;) {
+    if (_earlyMorningCheckBox[i].checked == true) {
+      // row
+      var addRow = _normalDeliveryInfoTable.insertRow(_normalDeliveryInfoTable.rows.length);
+      addRow.className += "normalContent"; // row가 추가될 때마다 해당 row에 content라는 class 명을 붙여준다.
 
+      // cell_1 :: checkbox :: 체크된 채로 생성
+      var cell1= addRow.insertCell(0);
+      let addCheckbox = document.createElement("input");
+      addCheckbox.className = "normalDeliveryInfoTableCheckBox";
+      addCheckbox.type = "checkbox";
+      addCheckbox.checked = true;
+      addCheckbox.addEventListener('click', function() {
+        isAllCheck(_normalCheckBox, _normalAllCheck);
+        sumTotalPrice(_normalCheckBox, _normalContent, _normalTotalPrice);
+      })
+      cell1.appendChild(addCheckbox);
+
+
+      // cell_2 :: 상품 이미지
+      var cell2 = addRow.insertCell(1);
+      cell2.innerHTML = _earlyMorningContent[i].children[1].innerHTML;
+
+      // cell_3 :: 상품 이름
+      var cell3 = addRow.insertCell(2);
+      cell3.innerHTML = _earlyMorningContent[i].children[2].innerText;
+
+      // cell_4 :: 상품 개별 가격
+      var cell4 = addRow.insertCell(3);
+      cell4.innerHTML = _earlyMorningContent[i].children[3].innerText;
+
+      // cell_5 :: 상품 개수
+      var cell5 = addRow.insertCell(4);
+      cell5.innerHTML = _earlyMorningContent[i].children[4].innerText;
+
+      // cell_6 :: 삼품 구매 가격
+      var cell6 = addRow.insertCell(5);
+      cell6.innerHTML = parseInt(cell4.innerText) * parseInt(cell5.innerText);
+
+      // 전체선택 체크
+      isAllCheck(_normalCheckBox, _normalAllCheck);
+      sumTotalPrice(_normalCheckBox, _normalContent, _normalTotalPrice);
+
+      _earlyMorningDeliveryInfoTable.deleteRow(i);
+    } else {
+      i++;
+    }
+  }
+}
+
+
+// 검색하여 해당 조건을 만족하는 콘텐츠의 style을 바꾸는 함수
+function searchProducts() {
+  for (var i = 0; i < _normalContent.length; i++) {
+
+    if (_textBoxInputProductsName.value == "" && _textBoxInputMinPrice.value == "" && _textBoxInputMaxPrice.value == "") { // 세가 다 빈칸
+      break;
+    }
+    if (_normalContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && _textBoxInputMinPrice.value == "" && _textBoxInputMaxPrice.value == "") { // 이름만 입력했을 때.
+
+      _normalContent[i].children[2].style.color = '#ff0000';
+      _normalContent[i].children[3].style.color = '#ff0000';
+      _normalContent[i].children[4].style.color = '#ff0000';
+      _normalContent[i].children[5].style.color = '#ff0000';
+
+      _normalContent[i].children[2].style.fontSize = '20px';
+      _normalContent[i].children[3].style.fontSize = '20px';
+      _normalContent[i].children[4].style.fontSize = '20px';
+      _normalContent[i].children[5].style.fontSize = '20px';
+
+      _normalContent[i].children[2].style.fontWeight = '600';
+      _normalContent[i].children[3].style.fontWeight = '600';
+      _normalContent[i].children[4].style.fontWeight = '600';
+      _normalContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_normalContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && parseInt(_textBoxInputMinPrice.value) <= parseInt(_normalContent[i].children[3].innerText) && _textBoxInputMaxPrice.value == "") { // 최소값만 입력했을 때.
+
+      _normalContent[i].children[2].style.color = '#ff0000';
+      _normalContent[i].children[3].style.color = '#ff0000';
+      _normalContent[i].children[4].style.color = '#ff0000';
+      _normalContent[i].children[5].style.color = '#ff0000';
+
+      _normalContent[i].children[2].style.fontSize = '20px';
+      _normalContent[i].children[3].style.fontSize = '20px';
+      _normalContent[i].children[4].style.fontSize = '20px';
+      _normalContent[i].children[5].style.fontSize = '20px';
+
+      _normalContent[i].children[2].style.fontWeight = '600';
+      _normalContent[i].children[3].style.fontWeight = '600';
+      _normalContent[i].children[4].style.fontWeight = '600';
+      _normalContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_normalContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && _textBoxInputMinPrice.value == "" && parseInt(_textBoxInputMaxPrice.value) >= parseInt(_normalContent[i].children[3].innerText)) { //최대값만 입력했을 때
+
+      _normalContent[i].children[2].style.color = '#ff0000';
+      _normalContent[i].children[3].style.color = '#ff0000';
+      _normalContent[i].children[4].style.color = '#ff0000';
+      _normalContent[i].children[5].style.color = '#ff0000';
+
+      _normalContent[i].children[2].style.fontSize = '20px';
+      _normalContent[i].children[3].style.fontSize = '20px';
+      _normalContent[i].children[4].style.fontSize = '20px';
+      _normalContent[i].children[5].style.fontSize = '20px';
+
+      _normalContent[i].children[2].style.fontWeight = '600';
+      _normalContent[i].children[3].style.fontWeight = '600';
+      _normalContent[i].children[4].style.fontWeight = '600';
+      _normalContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_normalContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && parseInt(_textBoxInputMinPrice.value) <= parseInt(_normalContent[i].children[3].innerText) && parseInt(_textBoxInputMaxPrice.value) >= parseInt(_normalContent[i].children[3].innerText)) { // 세개다 입력했을 때
+
+      _normalContent[i].children[2].style.color = '#ff0000';
+      _normalContent[i].children[3].style.color = '#ff0000';
+      _normalContent[i].children[4].style.color = '#ff0000';
+      _normalContent[i].children[5].style.color = '#ff0000';
+
+      _normalContent[i].children[2].style.fontSize = '20px';
+      _normalContent[i].children[3].style.fontSize = '20px';
+      _normalContent[i].children[4].style.fontSize = '20px';
+      _normalContent[i].children[5].style.fontSize = '20px';
+
+      _normalContent[i].children[2].style.fontWeight = '600';
+      _normalContent[i].children[3].style.fontWeight = '600';
+      _normalContent[i].children[4].style.fontWeight = '600';
+      _normalContent[i].children[5].style.fontWeight = '600';
+    }
+  }
+
+
+  for (var i = 0; i < _earlyMorningContent.length; i++) {
+
+    if (_textBoxInputProductsName.value == "" && _textBoxInputMinPrice.value == "" && _textBoxInputMaxPrice.value == "") { // 세개 다 빈칸
+      break;
+    }
+
+    if (_earlyMorningContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && _textBoxInputMinPrice.value == "" && _textBoxInputMaxPrice.value == "") { // 이름만 입력
+
+      _earlyMorningContent[i].children[2].style.color = '#ff0000';
+      _earlyMorningContent[i].children[3].style.color = '#ff0000';
+      _earlyMorningContent[i].children[4].style.color = '#ff0000';
+      _earlyMorningContent[i].children[5].style.color = '#ff0000';
+
+      _earlyMorningContent[i].children[2].style.fontSize = '20px';
+      _earlyMorningContent[i].children[3].style.fontSize = '20px';
+      _earlyMorningContent[i].children[4].style.fontSize = '20px';
+      _earlyMorningContent[i].children[5].style.fontSize = '20px';
+
+      _earlyMorningContent[i].children[2].style.fontWeight = '600';
+      _earlyMorningContent[i].children[3].style.fontWeight = '600';
+      _earlyMorningContent[i].children[4].style.fontWeight = '600';
+      _earlyMorningContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_earlyMorningContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && parseInt(_textBoxInputMinPrice.value) <= parseInt(_earlyMorningContent[i].children[3].innerText) && _textBoxInputMaxPrice.value == "") { // 최소값만 입력했을 때.
+
+      _earlyMorningContent[i].children[2].style.color = '#ff0000';
+      _earlyMorningContent[i].children[3].style.color = '#ff0000';
+      _earlyMorningContent[i].children[4].style.color = '#ff0000';
+      _earlyMorningContent[i].children[5].style.color = '#ff0000';
+
+      _earlyMorningContent[i].children[2].style.fontSize = '20px';
+      _earlyMorningContent[i].children[3].style.fontSize = '20px';
+      _earlyMorningContent[i].children[4].style.fontSize = '20px';
+      _earlyMorningContent[i].children[5].style.fontSize = '20px';
+
+      _earlyMorningContent[i].children[2].style.fontWeight = '600';
+      _earlyMorningContent[i].children[3].style.fontWeight = '600';
+      _earlyMorningContent[i].children[4].style.fontWeight = '600';
+      _earlyMorningContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_earlyMorningContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && _textBoxInputMinPrice.value == "" && parseInt(_textBoxInputMaxPrice.value) >= parseInt(_earlyMorningContent[i].children[3].innerText)) { //최대값만 입력했을 때
+
+      _earlyMorningContent[i].children[2].style.color = '#ff0000';
+      _earlyMorningContent[i].children[3].style.color = '#ff0000';
+      _earlyMorningContent[i].children[4].style.color = '#ff0000';
+      _earlyMorningContent[i].children[5].style.color = '#ff0000';
+
+      _earlyMorningContent[i].children[2].style.fontSize = '20px';
+      _earlyMorningContent[i].children[3].style.fontSize = '20px';
+      _earlyMorningContent[i].children[4].style.fontSize = '20px';
+      _earlyMorningContent[i].children[5].style.fontSize = '20px';
+
+      _earlyMorningContent[i].children[2].style.fontWeight = '600';
+      _earlyMorningContent[i].children[3].style.fontWeight = '600';
+      _earlyMorningContent[i].children[4].style.fontWeight = '600';
+      _earlyMorningContent[i].children[5].style.fontWeight = '600';
+
+    } else if (_earlyMorningContent[i].children[2].innerText.includes(_textBoxInputProductsName.value) && parseInt(_textBoxInputMinPrice.value) <= parseInt(_earlyMorningContent[i].children[3].innerText) && parseInt(_textBoxInputMaxPrice.value) >= parseInt(_earlyMorningContent[i].children[3].innerText)) { // 세개다 입력했을 때
+
+      _earlyMorningContent[i].children[2].style.color = '#ff0000';
+      _earlyMorningContent[i].children[3].style.color = '#ff0000';
+      _earlyMorningContent[i].children[4].style.color = '#ff0000';
+      _earlyMorningContent[i].children[5].style.color = '#ff0000';
+
+      _earlyMorningContent[i].children[2].style.fontSize = '20px';
+      _earlyMorningContent[i].children[3].style.fontSize = '20px';
+      _earlyMorningContent[i].children[4].style.fontSize = '20px';
+      _earlyMorningContent[i].children[5].style.fontSize = '20px';
+
+      _earlyMorningContent[i].children[2].style.fontWeight = '600';
+      _earlyMorningContent[i].children[3].style.fontWeight = '600';
+      _earlyMorningContent[i].children[4].style.fontWeight = '600';
+      _earlyMorningContent[i].children[5].style.fontWeight = '600';
+    }
+  }
+}
+
+
+// 검색하여 바꾼 스타일을 다시 되돌리는 함수 (모두 default값으로 초기값)
+function backToNormal() {
+  for (var i = 0; i < _normalContent.length; i++) {
+    _normalContent[i].children[2].style.color = 'black';
+    _normalContent[i].children[3].style.color = 'black';
+    _normalContent[i].children[4].style.color = 'black';
+    _normalContent[i].children[5].style.color = 'black';
+
+    _normalContent[i].children[2].style.fontSize = '16px';
+    _normalContent[i].children[3].style.fontSize = '16px';
+    _normalContent[i].children[4].style.fontSize = '16px';
+    _normalContent[i].children[5].style.fontSize = '16px';
+
+    _normalContent[i].children[2].style.fontWeight = '400';
+    _normalContent[i].children[3].style.fontWeight = '400';
+    _normalContent[i].children[4].style.fontWeight = '400';
+    _normalContent[i].children[5].style.fontWeight = '400';
+
+  }
+  for (var i = 0; i < _earlyMorningContent.length; i++) {
+    _earlyMorningContent[i].children[2].style.color = 'black';
+    _earlyMorningContent[i].children[3].style.color = 'black';
+    _earlyMorningContent[i].children[4].style.color = 'black';
+    _earlyMorningContent[i].children[5].style.color = 'black';
+
+    _earlyMorningContent[i].children[2].style.fontSize = '16px';
+    _earlyMorningContent[i].children[3].style.fontSize = '16px';
+    _earlyMorningContent[i].children[4].style.fontSize = '16px';
+    _earlyMorningContent[i].children[5].style.fontSize = '16px';
+
+    _earlyMorningContent[i].children[2].style.fontWeight = '400';
+    _earlyMorningContent[i].children[3].style.fontWeight = '400';
+    _earlyMorningContent[i].children[4].style.fontWeight = '400';
+    _earlyMorningContent[i].children[5].style.fontWeight = '400';
+
+  }
 }
